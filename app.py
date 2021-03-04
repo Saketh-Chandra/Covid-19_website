8d77eac8e11f00d3f33f32cd9ff454e66c2c2a0b
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-03-04 14:30:02 +0530

    optimized for the mobile viewing

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def data_in(Country, State=None):
 @app.route('/world_map', methods=['GET'])
 def world_wide():
     list_of_data = data_file.data_of_world_wide()
-    index=[['Country','Confirmed','Recovered']]
+    index = [['Country', 'Confirmed', 'Recovered']]
     for i in list_of_data:
         index.append(i)
     data_json = json.dumps(index)
     # print(index[0:2], type(index))
 
-    return render_template('worldwide.html',list_data=data_json)
+    return render_template('worldwide.html', list_data=data_json)
 
 
 @app.route('/', methods=['GET'])
@@ -61,37 +61,43 @@ def index2():
 
 @app.route('/country/state', methods=['GET', 'POST'])
 def index3():
-    if request.method == 'POST':
-        country = request.form['country']
-        state = request.form['state']
-        # print(country, state)
-        return redirect(url_for('home_view', Country=country, State=state))
-
-    return "POST"
+    try:
+        if request.method == 'POST':
+            country = request.form['country']
+            state = request.form['state']
+            # print(country, state)
+            return redirect(url_for('home_view', Country=country, State=state))
+        else:
+            return render_template('404_error.html')
+    except:
+        return render_template('404_error.html')
 
 
 @app.route('/data/', methods=["GET"])
 def home_view():
-    Country = str(request.args.get('Country')).title()
-    State = str(request.args.get('State')).title()
+    try:
+        Country = str(request.args.get('Country')).title()
+        State = str(request.args.get('State')).title()
 
-    if Country == "None":
-        Country = 'India'
-        State = ''
-    if State == "None":
-        State = ''
+        if Country == "None":
+            Country = 'India'
+            State = ''
+        if State == "None":
+            State = ''
 
-    data_arr = data_in(Country, State)
-    # data_arr = data_in("India", '')
+        data_arr = data_in(Country, State)
+        # data_arr = data_in("India", '')
 
-    # print(data_arr[0][-1])
+        # print(data_arr[0][-1])
 
-    index = json.dumps(data_arr[0])
-    data_c = json.dumps(data_arr[1])
-    data_r = json.dumps(data_arr[2])
-    data_d = json.dumps(data_arr[3])
-    return render_template('index.html', in_list_index=index, in_list_data_c=data_c, in_list_data_r=data_r,
-                           in_list_data_d=data_d, Country=Country, State=State)
+        index = json.dumps(data_arr[0])
+        data_c = json.dumps(data_arr[1])
+        data_r = json.dumps(data_arr[2])
+        data_d = json.dumps(data_arr[3])
+        return render_template('index.html', in_list_index=index, in_list_data_c=data_c, in_list_data_r=data_r,
+                               in_list_data_d=data_d, Country=Country, State=State)
+    except:
+        return render_template('404_error.html')
 
 
 @app.route('/api', methods=['GET'])
@@ -135,7 +141,7 @@ def new_home():
     index, c, r, d = data_file.new_cases_c(Country=Country, State=State)
     # print(index[-1], c[-1], r[-1], d[-1])
     return render_template('new.html', data_c=json.dumps(c), data_r=json.dumps(r), data_d=json.dumps(d),
-                           index=json.dumps(index),Country=Country, State=State)
+                           index=json.dumps(index), Country=Country, State=State)
 
 
 if __name__ == '__main__':
